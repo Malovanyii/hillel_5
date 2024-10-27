9857c3cc75be2ec277898cd8d90b927c73f7923f
Malovanyi <malovaniyy@gmail.com>
2024-10-27 21:04:25 +0200

    5.4

--- index.js
+++ index.js
@@ -12,17 +12,34 @@
 // }
 
 // HW 5.3. Пошук потрібних чисел
-while (true) {
-    N = prompt("Введіть ціле число N:");
+// while (true) {
+//     N = prompt("Введіть ціле число N:");
 
-    if (N !== null && Number.isInteger(parseFloat(N))) {
-        N = parseInt(N);
-        for (let i = 1; i <= 100; i++) {
-            if (i * i > N) break;
-            console.log(i);
+//     if (N !== null && Number.isInteger(parseFloat(N))) {
+//         N = parseInt(N);
+//         for (let i = 1; i <= 100; i++) {
+//             if (i * i > N) break;
+//             console.log(i);
+//         }
+//         break;
+//     } else {
+//         alert("Будь ласка, введіть ціле число.");
+//     }
+// }
+
+// HW 5.4. Просте число чи ні
+let num = prompt('Введіть ціле число');
+
+if (num > 1) {
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
+            console.log(`Число ${num} не є простим`);
+            break;
+        } else {
+            console.log(`Число ${num} є простим`);
+
         }
-        break;
-    } else {
-        alert("Будь ласка, введіть ціле число.");
     }
+} else {
+    console.log(`Число повинно бути більше 1`);
 }
