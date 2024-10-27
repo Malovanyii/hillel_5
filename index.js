034911c73cb67448c22c41a62bb5386cb878743a
Malovanyi <malovaniyy@gmail.com>
2024-10-27 21:03:06 +0200

    5.3

--- index.js
+++ index.js
@@ -4,9 +4,25 @@
 // }
 
 // HW 5.2. Розрахунок валюти
-const dollarInHryvnia = 26;
+// const dollarInHryvnia = 26;
 
-for (let value = 10; value <= 100; value += 10) {
-    let result = value * dollarInHryvnia;
-    console.log(`${value} доларів = ${result} гривень`);
+// for (let value = 10; value <= 100; value += 10) {
+//     let result = value * dollarInHryvnia;
+//     console.log(`${value} доларів = ${result} гривень`);
+// }
+
+// HW 5.3. Пошук потрібних чисел
+while (true) {
+    N = prompt("Введіть ціле число N:");
+
+    if (N !== null && Number.isInteger(parseFloat(N))) {
+        N = parseInt(N);
+        for (let i = 1; i <= 100; i++) {
+            if (i * i > N) break;
+            console.log(i);
+        }
+        break;
+    } else {
+        alert("Будь ласка, введіть ціле число.");
+    }
 }
