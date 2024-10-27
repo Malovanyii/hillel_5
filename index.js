bf0b62bc0560404c33e98f585fb16bf0d2ab8b39
Malovanyi <malovaniyy@gmail.com>
2024-10-27 21:01:34 +0200

    5.2

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // HW 5.1. Вивід чисел до консолі
-for (let i = 20; i <= 30; i += 0.5) {
-    console.log(i);
+// for (let i = 20; i <= 30; i += 0.5) {
+//     console.log(i);
+// }
+
+// HW 5.2. Розрахунок валюти
+const dollarInHryvnia = 26;
+
+for (let value = 10; value <= 100; value += 10) {
+    let result = value * dollarInHryvnia;
+    console.log(`${value} доларів = ${result} гривень`);
 }
